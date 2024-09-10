bf8fe27044d377d6ab74a5a89a8a24de282b9bf8
raylopez509 <raylopez509@gmail.com>
2024-09-10 11:55:04 -0700

    converted title to a component
    created title component

---
+++ src/app/components/Title.tsx
@@ -0,0 +1,11 @@
+import styles from "./title.module.css";
+
+export default function Title() {
+  return (
+    <>
+      <div className={styles.wrapper}>
+        <div className={styles.typing}>i rayn over this domain.</div>
+      </div>
+    </>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,16 @@
-import styles from "./page.module.css";
+// import styles from "./page.module.css";
 import Link from "next/link";
+import Title from "./components/Title";
 
 const links = [{ href: "/projects", text: "Projects" }];
 
 export default function Home() {
   return (
     <main>
-      <div className={styles.wrapper}>
+      {/* <div className={styles.wrapper}>
         <div className={styles.typing}>i rayn over this domain.</div>
-      </div>
+      </div> */}
+      <Title />
       <div>
         {links.map((link) => {
           return <Link href={link.href}>{link.text}</Link>;
