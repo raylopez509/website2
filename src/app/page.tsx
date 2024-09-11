250a0b899f33ef969ae01628db596f8a18d19383
raylopez509 <raylopez509@gmail.com>
2024-09-11 15:01:12 -0700

    testing out onclick on Project text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,33 @@
 // import styles from "./page.module.css";
+"use client";
+
 import Link from "next/link";
 import Title from "./components/Title";
+import { useState } from "react";
 
 const links = [{ href: "/projects", text: "Projects" }];
 
 export default function Home() {
+  const [move, setMove] = useState(false);
+
+  const handleClick = () => {
+    setMove(!move);
+    console.log("i got clicked!");
+  };
+
   return (
     <main>
       {/* <div className={styles.wrapper}>
         <div className={styles.typing}>i rayn over this domain.</div>
       </div> */}
       <Title />
-      <div>
+      {/* <div>
         {links.map((link) => {
           return <Link href={link.href}>{link.text}</Link>;
         })}
+      </div> */}
+      <div className={`box ${move ? "move" : ""}`} onClick={handleClick}>
+        Projects
       </div>
     </main>
   );
