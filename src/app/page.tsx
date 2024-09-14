b36d26af47d8c5a93a924f1c28ec721271febab4
raylopez509 <raylopez509@gmail.com>
2024-09-13 23:14:08 -0700

    Got the links to start moving on click
    Link now moves on click. Now I just need to set the title to be smaller when the first click happens

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
-// import styles from "./page.module.css";
 "use client";
 
+import styles from "./page.module.css";
 import Link from "next/link";
 import Title from "./components/Title";
 import { useState } from "react";
@@ -12,21 +12,15 @@ export default function Home() {
 
   const handleClick = () => {
     setMove(!move);
-    console.log("i got clicked!");
   };
 
   return (
     <main>
-      {/* <div className={styles.wrapper}>
-        <div className={styles.typing}>i rayn over this domain.</div>
-      </div> */}
       <Title />
-      {/* <div>
-        {links.map((link) => {
-          return <Link href={link.href}>{link.text}</Link>;
-        })}
-      </div> */}
-      <div className={`box ${move ? "move" : ""}`} onClick={handleClick}>
+      <div
+        className={`${styles.box} ${move ? styles.move : ""}`}
+        onClick={handleClick}
+      >
         Projects
       </div>
     </main>
