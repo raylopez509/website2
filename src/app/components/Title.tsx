efac2e84763327d87aaada7d07d285d56e795c0b
raylopez509 <raylopez509@gmail.com>
2024-09-19 17:42:15 -0700

    added transition classnames to the Title
    moved the transition code to the Title component

--- src/app/components/Title.tsx
+++ src/app/components/Title.tsx
@@ -1,9 +1,9 @@
 import styles from "./title.module.css";
 
-export default function Title() {
+export default function Title({ move }) {
   return (
     <>
-      <div className={styles.wrapper}>
+      <div className={`${styles.wrapper} ${move ? styles.clicked : ""}`}>
         <div className={styles.typing}>i rayn over this domain.</div>
       </div>
     </>
