d0d41287fcaaabc0ddf63840a608280eaf131507
raylopez509 <raylopez509@gmail.com>
2024-09-19 19:08:10 -0700

    changed move variables to be clearer and replaced the test project link to a navbar
    Changed the move and setmove to isClicked and setIsClicked
    Also changed out the test project link to a navbar with links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,26 +3,23 @@
 import styles from "./page.module.css";
 import Link from "next/link";
 import Title from "./components/Title";
+import NavBar from "./components/NavBar";
 import { useState } from "react";
 
 const links = [{ href: "/projects", text: "Projects" }];
 
 export default function Home() {
-  const [move, setMove] = useState(false);
+  const [isClicked, setIsClicked] = useState(false);
 
   const handleClick = () => {
-    setMove(true);
+    setIsClicked(true);
   };
 
   return (
     <main>
-      <Title move={move} />
-      <div
-        // className={`${styles.box} ${move ? styles.move : ""}`}
-        onClick={handleClick}
-      >
-        Projects
-      </div>
+      <Title clicked={isClicked} />
+      <NavBar />
+      {/* <div onClick={handleClick}>Projects</div> */}
     </main>
   );
 }
