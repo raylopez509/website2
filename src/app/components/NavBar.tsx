9a996cd7557a7e68eb922ca1fa08a3bd6c4a6ec8
raylopez509 <raylopez509@gmail.com>
2024-09-19 19:02:46 -0700

    Copied over the navbar from old website project
    NavBar is just the links in the navigation bar instead of the title too

---
+++ src/app/components/NavBar.tsx
@@ -0,0 +1,52 @@
+import styles from "./navbar.module.css";
+import Link from "next/link";
+
+const links = [
+  {
+    href: "/",
+    text: "Home",
+  },
+  {
+    href: "/projects",
+    text: "Projects",
+  },
+  {
+    href: "/tictactoe",
+    text: "Tic Tac Toe",
+  },
+  {
+    href: "/firstperson",
+    text: "First Person Selector",
+  },
+  {
+    href: "/teamrandomizer",
+    text: "Team Randomizer",
+  },
+  {
+    href: "top10boardgames",
+    text: "Top 10 Board Games",
+  },
+  // {
+  //   href: '#',
+  //   text: 'Board Game Collection',
+  // },
+];
+
+export default function NavigationBar() {
+  return (
+    <>
+      <div className={styles.navbar}>
+        {links.map((link, index) => {
+          const linkStyle = {
+            animationDelay: `${4 + index}s`,
+          };
+          return (
+            <Link className={styles.slidein} style={linkStyle} href={link.href}>
+              {link.text}
+            </Link>
+          );
+        })}
+      </div>
+    </>
+  );
+}
