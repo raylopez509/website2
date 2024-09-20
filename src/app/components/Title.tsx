a66e07ed475fabc39a62ca6b7da08d26207cd85f
raylopez509 <raylopez509@gmail.com>
2024-09-19 19:03:44 -0700

    changed move to clicked variable
    This makes it clearer on what that variable does

--- src/app/components/Title.tsx
+++ src/app/components/Title.tsx
@@ -1,9 +1,9 @@
 import styles from "./title.module.css";
 
-export default function Title({ move }) {
+export default function Title({ clicked }: { clicked: any }) {
   return (
     <>
-      <div className={`${styles.wrapper} ${move ? styles.clicked : ""}`}>
+      <div className={`${styles.wrapper} ${clicked ? styles.clicked : ""}`}>
         <div className={styles.typing}>i rayn over this domain.</div>
       </div>
     </>
