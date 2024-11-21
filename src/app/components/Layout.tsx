a847ca0b1094ed909321ed5a73e551ae99c2fe0e
raylopez509 <raylopez509@gmail.com>
2024-11-20 23:35:35 -0800

    made layout stay small if they navigated to the links directly
    Without this code, the title would stay large even if they navigated directly to like /projects. With this code, the title will shrink if they are not on the home screen.

--- src/app/components/Layout.tsx
+++ src/app/components/Layout.tsx
@@ -2,14 +2,23 @@
 
 import Title from "./Title";
 import NavBar from "./NavBar";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { usePathname } from "next/navigation";
 
 export default function Layout() {
+  const pathname = usePathname();
   const [isClicked, setIsClicked] = useState(false);
-
   const handleClick = () => {
     setIsClicked(true);
   };
+  useEffect(() => {
+    if (pathname === "/") {
+      setIsClicked(false);
+    } else {
+      setIsClicked(true);
+    }
+  });
+
   return (
     <>
       <Title clicked={isClicked} />
